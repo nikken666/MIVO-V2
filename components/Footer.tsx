3ee93d7f8bad75b2752509bab38d024a4c6296df
nikken666 <oonzhenlun@gmail.com>
2026-09-24 13:16:33 +0800

    Route footer Track Order to customer orders

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
           <p>Premium automotive parts, matched with more confidence.</p>
         </div>
         <div><h4>SHOP</h4><Link href="/products">All Parts</Link><Link href="/brands">Brands</Link><Link href="/garage">My Garage</Link></div>
-        <div><h4>SUPPORT</h4><Link href="/track-order">Track Order</Link><Link href="/help">Help Centre</Link><Link href="/account">My Account</Link></div>
+        <div><h4>SUPPORT</h4><Link href="/orders">Track Order</Link><Link href="/help">Help Centre</Link><Link href="/account">My Account</Link></div>
         <div><h4>MIVO</h4><Link href="/#fitment">Vehicle Match</Link><Link href="/#standard">Our Standard</Link></div>
       </div>
       <div className="container footerBottom">
