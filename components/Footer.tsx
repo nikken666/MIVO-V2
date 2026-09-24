cfd67649ce4daa780056c498e1bbc5750be5510b
nikken666 <oonzhenlun@gmail.com>
2026-09-24 12:42:11 +0800

    Remove Seller Centre from footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,7 +11,7 @@ export default function Footer() {
         </div>
         <div><h4>SHOP</h4><Link href="/products">All Parts</Link><Link href="/brands">Brands</Link><Link href="/garage">My Garage</Link></div>
         <div><h4>SUPPORT</h4><Link href="/track-order">Track Order</Link><Link href="/help">Help Centre</Link><Link href="/account">My Account</Link></div>
-        <div><h4>MIVO</h4><Link href="/#fitment">Vehicle Match</Link><Link href="/sellers">Seller Centre</Link><Link href="/#standard">Our Standard</Link></div>
+        <div><h4>MIVO</h4><Link href="/#fitment">Vehicle Match</Link><Link href="/#standard">Our Standard</Link></div>
       </div>
       <div className="container footerBottom">
         <span>© 2026 MIVO. All rights reserved.</span>
