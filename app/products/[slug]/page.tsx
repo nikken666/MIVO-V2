5e4bb9cb46eccb94ac2585c753ebd79a900a92c4
nikken666 <oonzhenlun@gmail.com>
2026-09-26 17:43:46 +0800

    Always refresh product rating data

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { notFound } from "next/navigation";
 import ProductDetailClient from "@/components/ProductDetailClient";
 import { getActiveProductBySlug } from "@/lib/catalog";
